6b3d986b6b4b016cac6a6690a8be5d40d4ed53a3
Lola Onanuga <slonanuga@gmail.com>
2018-09-27 22:46:53 +0000

    complete

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
-def my_select(collection)
- # your code here!
+def my_select(collection) {}
+  if collection.size == 0
+    puts "nothing entered"
+  end
+ count = 0
+ new_array = []
+ while count < collection.size
+ count +=1
+if yield(collection[count-1]) == true
+ new_array.push(collection[count-1])
+end
+end
+new_array
 end
