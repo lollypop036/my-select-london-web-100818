fe58161b68e9bca1d9754598cf3f3e8c66565a19
Lola Onanuga <slonanuga@gmail.com>
2018-09-27 22:45:54 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -6,8 +6,8 @@ def my_select(collection) {}
  new_array = []
  while count < collection.size
  count +=1
-if yield(collection[count-1])) == true
- new_array.push
+if yield(collection[count-1]) == true
+ new_array.push(collection[count-1])
 end
 new_array
 end
